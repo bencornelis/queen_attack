ee32c1f3b3bcf19f97f270a0f6d64450da8308e2
Ben Cornelis & Jesse James <student@epicodus.com>
2015-04-20 11:34:21 -0700

    Write second test and pass.

--- lib/queen_attack.rb
+++ lib/queen_attack.rb
@@ -1,5 +1,13 @@
 class Array
   define_method(:queen_attack?) do |coords|
-    false
+    queen_x = self.at(0)
+    queen_y = self.at(1)
+    opponent_x = coords.at(0)
+    opponent_y = coords.at(1)
+    if queen_y.eql?(opponent_y)
+      true
+    else
+      false
+    end
   end
 end
