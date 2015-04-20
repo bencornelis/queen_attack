951ddbe042153899b879c08d599782d75dc3b650
Ben Cornelis & Jesse James <student@epicodus.com>
2015-04-20 11:58:10 -0700

    Refactored queen_attack? method.

--- lib/queen_attack.rb
+++ lib/queen_attack.rb
@@ -4,12 +4,20 @@ class Array
     queen_y = self.at(1)
     opponent_x = coords.at(0)
     opponent_y = coords.at(1)
-    if queen_y.eql?(opponent_y).|(queen_x.eql?(opponent_x))
-      true
-    elsif ((((opponent_y - queen_y) / (opponent_x - queen_x)).abs).eql?(1))
+    horizontally_aligned = queen_y.eql?(opponent_y)
+    vertically_aligned = queen_x.eql?(opponent_x)
+    if horizontally_aligned.|(vertically_aligned)
       true
     else
-      false
+      slope = (opponent_y - queen_y) / (opponent_x - queen_x)
+      diagonally_aligned = slope.abs().eql?(1)
     end
   end
 end
+
+=begin
+elsif ((((opponent_y - queen_y) / (opponent_x - queen_x)).abs).eql?(1))
+  true
+else
+  false
+=end
